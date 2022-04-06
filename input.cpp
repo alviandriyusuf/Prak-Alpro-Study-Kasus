0c992ddf091342c74f8e0f5939a002b921b9b80c
alviandriyusuf <101722965+alviandriyusuf@users.noreply.github.com>
2022-04-06 23:40:00 +0700

    Dev

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include"../library/input.h"
+int main(){
+	Input input;
+	input.cetak();
+	input.tofile();
+	return 0;
+}
---
+++ proses.cpp
@@ -0,0 +1,9 @@
+#include "../library/proses.h"
+#include <fstream>
+#include <iostream>
+int main() {
+  Proses proses;
+  proses.getData();
+  proses.toFile();
+  return 0;
+}
